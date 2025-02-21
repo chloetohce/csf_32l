da64591ef8fb48a86765d95a13d5960a84790e87
chloetohce <chloetohce@gmail.com>
2025-02-21 12:16:05 +0800

    completed

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Task } from '../../models';
 
 @Component({
@@ -16,6 +16,8 @@ export class TaskComponent {
   // Form model
   // Generally, the top-level form is a form group
   protected form!: FormGroup
+
+  protected collaborators!: FormArray // define an array to hold the collaborators
 
   ngOnInit(): void {
     console.info("In ngOnInit")
@@ -35,8 +37,17 @@ export class TaskComponent {
     console.info("form: ", values)
   }
 
+  protected addCollaborator() {
+    // Create the collaborator FormGroup and add it to the array
+    this.collaborators.push(this.createCollaborator())
+  }
+
   protected isCtrlValid(attr: string) : boolean {
     return !!this.form.get(attr)?.valid
+  }
+
+  protected invalid() : boolean {
+    return !this.form.valid && !this.collaborators.valid
   }
   
   protected updateUrgency(event: any) {
@@ -44,13 +55,28 @@ export class TaskComponent {
   }
   
   private createForm(): FormGroup {
+    this.collaborators = this.fb.array([]) //Everytime we create the form, we recreate the collaborators array
+    // NOTE: Cannot just add validator here to check for min length of array. Does not check if the collaborator inside is valid. 
+    // Need to define another method to check for all validation. See invalid().
     return this.fb.group({
       taskName: this.fb.control<string>('', [Validators.required, Validators.minLength(3)]), // inside is initial value
       priority: this.fb.control<string>('2'),
       date: this.fb.control<string>('', [Validators.required]),
       urgency: this.fb.control<number>(3),
       comments: this.fb.control<string>(''),
-      procrastinate: this.fb.control<boolean>(false)
+      procrastinate: this.fb.control<boolean>(false),
+      collaborators: this.collaborators
     })
+  }
+
+  private createCollaborator(): FormGroup {
+    return this.fb.group({
+      name: this.fb.control<string>(''),
+      email: this.fb.control<string>('', [Validators.required])
+    })
+  }
+
+  protected removeCollaborator(idx: number) {
+    this.collaborators.removeAt(idx);
   }
 }
